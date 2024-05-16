0068537a71d55f4827bbec3af7bde9ec12b45199
Mikhail Shafigullin <mikhail.shafigullin@fundraiseup.com>
2024-05-16 12:35:51 +0200

    add some more tests with frontend redirect

---
+++ tests/locationReplace-1.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+for (let index = 0; index < 20; index++) {
+  test(`check 302 code ${index}`, async ({ page }) => {
+    await page.goto('https://mikhail-shafigullin.github.io/');
+    await page.getByRole('link', { name: 'location.replace' }).click();
+    await page.waitForURL(/github/, {waitUntil: 'load'});
+  });
+}
