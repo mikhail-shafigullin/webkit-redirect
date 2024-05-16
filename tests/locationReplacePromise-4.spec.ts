b268cfaffb6a24d7f45d17b92d8b241a73cd6dc3
Mikhail Shafigullin <mikhail.shafigullin@fundraiseup.com>
2024-05-16 13:12:21 +0200

    remove backend tests + add more tests for frontend

---
+++ tests/locationReplacePromise-4.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+for (let index = 0; index < 20; index++) {
+  test(`check location.replace inside of promise code 1-${index}`, async ({ page }) => {
+    await page.goto('https://mikhail-shafigullin.github.io/');
+    await page.locator('#test-promise-location-replace').click();
+    await page.waitForURL(/github/, {waitUntil: 'load'});
+  });
+}
