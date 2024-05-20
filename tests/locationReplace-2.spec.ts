6442b3f69be020fe8a9da2853c5a573e7ec4aedd
Mikhail Shafigullin <mikhail.shafigullin@fundraiseup.com>
2024-05-20 13:51:47 +0200

    try tests with timeout

--- tests/locationReplace-2.spec.ts
+++ tests/locationReplace-2.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 for (let index = 0; index < 20; index++) {
   test(`check location.replace code 2-${index}`, async ({ page }) => {
     await page.goto('https://mikhail-shafigullin.github.io/');
-    await page.locator('#test-location-replace').click();
+    await page.locator('#test-promise-location-replace-timeout-before').click();
     await page.waitForURL(/github/, {waitUntil: 'load'});
   });
 }
--- tests/locationReplacePromise-1.spec.ts
+++ tests/locationReplacePromise-1.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 for (let index = 0; index < 20; index++) {
   test(`check location.replace inside of promise code 1-${index}`, async ({ page }) => {
     await page.goto('https://mikhail-shafigullin.github.io/');
-    await page.locator('#test-promise-location-replace').click();
+    await page.locator('#test-promise-location-replace-timeout-after').click();
     await page.waitForURL(/github/, {waitUntil: 'load'});
   });
 }
